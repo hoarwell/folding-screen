d7fcfa80782c2a9da5a82c33626892cbf756bb8e
YoungJo Jeong <youngjo@YoungJos-MacBook-Air.local>
2021-06-14 16:37:09 +0900

    21.06.14. 16:36 각도가 줄어들면서 이미지 사이가 조금씩 가려짐.. 더 넓히던가 해야함

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React, { useState, useEffect } from 'react';
+import face from './face.png';
 import './App.css';
 
 function App() {
+  const [init, setInit] = useState({
+    odd: 3,
+    even: 5,
+  });
   let screenWidth = window.innerWidth;
 
   const handleResize = (e) => {
     let percentage = (e.target.innerWidth * 70) / screenWidth;
-    console.log(70 - percentage);
     const li = document.querySelectorAll('li');
       li.forEach((el, i) => {
-        if (i % 2 === 0) {
+        if (i % 2 === 0) { //  even
+          console.log(70 - percentage)
           el.style.transform = `skewY(-${70 - percentage}deg)`;
-        } else {
+        } else { // odd
+          console.log(70 - percentage)
           el.style.transform = `skewY(${70 - percentage}deg)`;
         }
       })
+  }
 
+  const initialize = () => {
+    const li = document.querySelectorAll('li');
+    li.forEach((el, i) => {
+      el.style.backgroundImage = `url(${face})`;
+      if (i % 2 === 0) { //  even
+        el.style.backgroundRepeat = "no-repeat";
+        if( i === 2) {
+          el.style.backgroundPositionX = `${init.even * 10}`;
+        }
+        el.style.backgroundPositionX = `${i * 20}%`;
+      } else { // odd
+        el.style.backgroundRepeat = "no-repeat";
+        if ( i === 0){
+          el.style.backgroundPositionX = `${init.odd}`;
+        }
+        el.style.backgroundPositionX = `${i * 20}%`;
+      }
+    })
   }
 
   useEffect(() => {
+    initialize();
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   })
