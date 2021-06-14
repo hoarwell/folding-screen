7ff980a5b7d7405eda000cec5cffcf65492d0462
YoungJo Jeong <youngjo@YoungJos-MacBook-Air.local>
2021-06-14 14:58:06 +0900

    21.06.14. 14:57 first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const handleResize = (e) => {
+    console.log(e.target.innerWidth);
+    const ul = document.querySelector('ul');
+    const li = document.querySelectorAll('li');
+      li.forEach((el, i) => {
+        console.log(el)
+        if (i % 2 === 0) {
+          el.style.transform = "skewY(50deg)";
+        } else {
+          el.style.transform = "skewY(-50deg)";
+        }
+      })
+
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 }
